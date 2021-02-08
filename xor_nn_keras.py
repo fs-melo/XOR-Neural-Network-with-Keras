94de77f01ce1622e35cffaf7b42807520f5858fc
fs-melo <fs-melo@bol.com.br>
2021-02-08 12:38:42 -0300

    Initial downloads

---
+++ xor_nn_keras.py
@@ -0,0 +1,37 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense
+import matplotlib.pyplot as plt
+
+# the four different states of the XOR gate
+training_data = np.array([[0,0],[0,1],[1,0],[1,1]], "float32")
+# the four expected results in the same order
+target_data = np.array([[0],[1],[1],[0]], "float32")
+
+model = Sequential()
+model.add(Dense(16, input_dim=2, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+# history = model.fit(training_data, target_data, validation_split=0.25, epochs=150, batch_size=10, verbose=0)
+history = model.fit(training_data, target_data, epochs=2000, batch_size=1)
+# list all data in history
+print(history.history.keys())
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+# plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+plt.show()
